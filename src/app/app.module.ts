977492b15bcaff83422eb7dbef9b830eb8e752cd
Mohamed Romdhani <mromdhani7@gmail.com>
2018-08-03 15:16:34 +0100

    Ajout de la fonctionalité du routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,29 @@ import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DetailsCompteComponent } from './details-compte/details-compte.component';
+
+const tableRoutage = [
+    {path: 'comptes', component: ContentComponent},
+    {path: 'comptes/:id', component: DetailsCompteComponent},
+    {path: '', component: WelcomeComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    DetailsCompteComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(tableRoutage)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/details-compte/details-compte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-compte',
+  templateUrl: './details-compte.component.html',
+  styleUrls: ['./details-compte.component.css']
+})
+export class DetailsCompteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
